e2615d92e2e5f47286e49e50bd710ae307acaa8e
dungvuong-tech <dung.vuong@ltcapital.partners>
2025-07-04 15:16:55 +0700

    update database connection for testing environment

--- src/config/settings/testing.py
+++ src/config/settings/testing.py
@@ -6,10 +6,5 @@ DEBUG = False
 # Load testing environment variables
 environ.Env.read_env(BASE_DIR.parent / ".envs" / "testing" / ".django")
 
-# Use in-memory SQLite database for testing
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": ":memory:",
-    }
-}
+# Use PostgreSQL for testing
+DATABASES = {"default": env.db()}
